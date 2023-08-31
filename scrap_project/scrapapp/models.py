0ae6ec60269391ab0c4ae2f4c15aa25c72337d05
vyshnavip1998 <118480225+vyshnavip1998@users.noreply.github.com>
2023-08-31 12:37:35 +0530

    first commit

---
+++ scrap_project/scrapapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Links(models.Model):
+    address=models.CharField(max_length=500,null=True,blank=True)
+    stringname=models.CharField(max_length=500,blank=True,null=True)
+    def __str__(self):
+        return self.stringname
+
+
---
+++ scrap_project/scrapapp/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from . models import Links
+
+# Create your views here.
+def home(request):
+    if request.method=="POST":
+        link_new=request.POST.get('page','')
+        urls=requests.get(link_new)
+        beauty=BeautifulSoup(urls.text,'html.parser')
+    # address=[]
+        for link in beauty.find_all('a'):
+            li_address=link.get('href')
+            li_name=link.string
+            Links.objects.create(address=li_address,stringname=li_name)
+        return HttpResponseRedirect('/')
+    else:
+        data_values=Links.objects.all()
+        # address.append(link.get('href'))
+
+    return render(request,'home.html',{'data_values':data_values})
